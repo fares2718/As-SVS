754b189cb635eaad368cb490c3380abefb9463c8
fares2718 <faresobaid2715@gmail.com>
2025-10-06 08:10:08 +0300

    Add some operations for admin on person

--- As-SVS/As-SVS.API/Controllers/AdminController.cs
+++ As-SVS/As-SVS.API/Controllers/AdminController.cs
@@ -19,6 +19,8 @@ namespace As_SVS.API.Controllers
             _adminServices = adminServices;
         }
 
+        #region OnPerson
+
         [HttpGet("GetAll",Name ="GetAllPeople")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -30,5 +32,62 @@ namespace As_SVS.API.Controllers
             return Ok(peopleList);
         }
 
+        [HttpGet("{Id}", Name = "GetPersonById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetPersonByIdAsync(int Id)
+        {
+            if (Id < 1)
+                return BadRequest($"ID {Id} is not valid");
+            var person = await _personSevices.GetByIdAsync(Id);
+            if (person == null) 
+                return NotFound($"Person with ID {Id} dose not exist");
+            return Ok(person);
+        }
+
+        [HttpPut("Update-Person/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdatePersonAsync(int Id, PersonDTO personDTO)
+        {
+            if (Id < 1)
+                return BadRequest($"ID {Id} is not valid");
+            var person = await _personSevices.GetByIdAsync(Id);
+            if (person == null)
+                return NotFound($"Person with ID {Id} dose not exist");
+            personDTO.Id = person.Id;
+            person.FirstName = personDTO.FirstName;
+            person.MiddleName = personDTO.MiddleName;
+            personDTO.LastName = personDTO.LastName;
+            person.Password = personDTO.Password;
+            person.Phone = personDTO.Phone;
+            if(await _personSevices.UpdateAsync(person))
+                return Ok(person);
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to update person");
+        }
+
+        [HttpDelete("Delete-Person/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeletePersonAsync(int Id)
+        {
+            if (Id < 1)
+                return BadRequest($"ID {Id} is not valid");
+            var person = await _personSevices.GetByIdAsync(Id);
+            if (person == null)
+                return NotFound($"Person with ID {Id} dose not exist");
+            if (await _personSevices.DeleteAsync(Id))
+                return Ok(person);
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to delete person");
+        }
+
+        #endregion
     }
 }
