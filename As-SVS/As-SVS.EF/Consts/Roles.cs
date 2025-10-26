64d6917327f8dedf1fb8ecde840b4b5dc96afe1b
fares2718 <faresobaid2715@gmail.com>
2025-10-26 21:14:43 +0300

    Seeded database wit needed roles

---
+++ As-SVS/As-SVS.EF/Consts/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace As_SVS.API.Helpers
+{
+    public class Roles
+    {
+        public const string None = "None";
+        public const string Admin = "Admin";
+        public const string Teacher = "Teacher";
+        public const string Student = "Student";
+    }
+}
---
+++ As-SVS/As-SVS.EF/Migrations/20251026175520_SeedRoles.cs
@@ -0,0 +1,46 @@
+﻿using As_SVS.API.Helpers;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+
+namespace AsSVS.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] {Guid.NewGuid().ToString(),Roles.None,Roles.None.ToUpper(), Guid.NewGuid().ToString() }
+            );
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), Roles.Admin, Roles.Admin.ToUpper(), Guid.NewGuid().ToString() }
+            );
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), Roles.Teacher, Roles.Teacher.ToUpper(), Guid.NewGuid().ToString() }
+            );
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), Roles.Student, Roles.Student.ToUpper(), Guid.NewGuid().ToString() }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [AspNetRoles]");
+        }
+    }
+}
