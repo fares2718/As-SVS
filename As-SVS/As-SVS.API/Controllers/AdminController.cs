b7a980c4e1a11ff780827b57ab9eea95e8758715
fares2718 <faresobaid2715@gmail.com>
2025-11-03 10:44:30 +0300

    Added admin gets

--- As-SVS/As-SVS.API/Controllers/AdminController.cs
+++ As-SVS/As-SVS.API/Controllers/AdminController.cs
@@ -13,10 +13,12 @@ namespace As_SVS.API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IAdminServices _adminServices;
+        private readonly ITeacherServices _teacherServices;
 
-        public AdminController(IAdminServices adminServices)
+        public AdminController(IAdminServices adminServices, ITeacherServices teacherServices)
         {
             _adminServices = adminServices;
+            _teacherServices = teacherServices;
         }
 
         [HttpPost("complete-profile/{userId}")]
@@ -31,6 +33,51 @@ namespace As_SVS.API.Controllers
                 return BadRequest("Invalid Data");
             int adminId = await _adminServices.AddNewAsync(admin,userId);
             return CreatedAtRoute($"complete-profile/{userId}",adminId);
+        }
+
+        [HttpGet("get-all-teachers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> GetAllTeachersAsync()
+        {
+            var teachers = await _teacherServices.GetAllAsync();
+            if (!teachers.Any())
+                return NotFound("No teacher was found");
+            return Ok(teachers);
+        }
+
+        [HttpGet("get-teacher/{teacherId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> GetTeacherByIdAsync(int teacherId)
+        {
+            if (teacherId < 1)
+                return BadRequest("Invalid Id");
+            var teacher = await _teacherServices.GetByIdAsync(teacherId);
+            if (teacher is null || string.IsNullOrEmpty(teacher.UserName))
+                return NotFound("No teacher was found");
+            return Ok(teacher);
+        }
+
+        [HttpGet("search-teacher/{name}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> SearchTeachersWithName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return BadRequest("Invalid name");
+            var teachers = await _teacherServices.SearchByNameAsync(name);
+            if (!teachers.Any())
+                return NotFound("No teacher found with this name");
+            return Ok(teachers);
         }
     }
 }
